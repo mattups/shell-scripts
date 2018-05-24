6969b5b3b330e4fcfa1e1a29b797aa77a8189f58
Mattia Tupone <tupone.mattia@gmail.com>
2018-05-24 18:25:06 +0200

    Formatted ps-gen.ps1 file

--- win-secure-password/pw-gen.ps1
+++ win-secure-password/pw-gen.ps1
@@ -1,6 +1,7 @@
 # SecureString password generation
-
 $plainTextPassword = "Plain text password"
+
 $securePassword = $plainTextPassword | ConvertTo-SecureString -AsPlainText -Force 
 $storedPassword = $securePassword | ConvertFrom-SecureString 
+
 Set-Content "secureStringPassword.txt" $storedPassword
